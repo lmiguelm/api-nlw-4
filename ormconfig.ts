4e254cf7e294412392a81b9ebb0e0dec4724caec
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2021-03-15 22:53:52 -0300

    Arrumando variaveis ambiente

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,12 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config({
-  path: process.env.NODE_ENV == 'development' ? '.env.development' : process.env.NODE_ENV == 'test' ? '.env.test' : 'env'
+  path: process.env.NODE_ENV == 'development' ? '.env.development' : process.env.NODE_ENV == 'test' ? '.env.test' : '.env'
 });
 
-export default {
-  type: process.env.DATABASE_TYPE as string,
+import { ConnectionOptions } from 'typeorm';
+
+const config: ConnectionOptions = {
+  type: process.env.DATABASE_TYPE as any,
   host: process.env.DATABASE_HOST as string,
-  port: process.env.DATABASE_PORT as string,
+  port: Number(process.env.DATABASE_PORT as string) || undefined,
   username: process.env.DATABASE_USERNAME as string,
   password: process.env.DATABASE_PASSWORD as string,
   database: process.env.DATABASE as string,
@@ -16,3 +18,5 @@ export default {
   migrations: ['./src/database/migrations/*.ts'],
   entities: ['./src/models/*.ts'],
 }
+
+export default config;
